02c51925ff16df7fa5ee8a8b6ac5fd05822ea629
Deepak <deepaklewiskumar.19@gmail.com>
2025-09-30 15:21:29 +0530

    fetch data

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import React from 'react'
+import Movies from './movies'
+function App() {
+  
+
+  return (
+    <>
+      <Movies/>
+    </>
+  )
+}
+
+export default App
---
+++ src/movies.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './movies.css'
+import {useState,useEffect} from 'react';
+
+
+
+export default function Movies() {
+    const [data,setdata]=useState([]);
+    const [search,setsearch]=useState("");
+    useEffect(()=>{
+        let ismounted=true;
+
+        const fetchData=async()=>{
+            const res=await fetch(`https://www.omdbapi.com/?s=${search}&apikey=7cd996c3`);
+            const result=await res.json();
+            console.log(result);
+            if(ismounted){
+            setdata(result.Search);
+            }
+        }
+        fetchData();
+        return()=>{ismounted=false;}
+    },[search]);
+
+    return (
+        <div>
+            <h1>Movies Page</h1>
+
+            <input type="text" placeholder='Search Movies' value={search} onChange={(e)=>setsearch(e.target.value)} />
+            {data && data.length>0?(
+                data.map((movie)=>(
+                    <div key={movie.imdbID} className='movie'>
+                        <h2>{movie.Title}</h2>
+                        <p>{movie.Year}</p>
+                        <img src={movie.Poster} alt={movie.Title} />
+                    </div>
+                ))
+            ):(
+                <p>No Movies Found</p>
+            )}
+            
+        </div>
+    )
+}
